b2bb6b192d9597d33d9a05b6c7c28a536066a419
baydre_africa <baydreafrica@gmail.com>
2023-10-13 19:33:06 +0100

    Added python-django-blog basics

---
+++ mysite/blog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, \
+                                    PageNotAnInteger
+from .forms import EmailPostForm
+#
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+
+    if request.methos == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post,
+                                                        'form': form})
+# Create your views here.
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 1) # 3 post in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliver th first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                'blog/post/list.html',
+                {'page': page,
+                'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                    status='published',
+                                    publish__year=year,
+                                    publish__month=month,
+                                    publish__day=day)
+    return render(request,
+                'blog/post/detail.html',
+                {'post':post}) 

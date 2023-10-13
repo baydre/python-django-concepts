f5e8f6c8ff4d9f44a9e4f83c1836d1e84e7a3688
baydre_africa <baydreafrica@gmail.com>
2023-10-13 20:20:34 +0100

    Added Sending emails with django section

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,22 +3,34 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, \
                                     PageNotAnInteger
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 #
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent =  False
 
-    if request.methos == 'POST':
+    if request.method== 'POST':
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
             # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com',
+                    [cd['to']])
+            sent = True
         else:
             form = EmailPostForm()
         return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form})
+                                                        'form': form,
+                                                        'sent': sent})
 # Create your views here.
 def post_list(request):
     object_list = Post.published.all()
